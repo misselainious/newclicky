52d837bcfe9c0984a2687aec778708e9b0e7dace
Elaine <elaine.cadman@gmail.com>
2019-02-13 14:07:24 -0800

    Updates

---
+++ precache-manifest.760a2a1eeb9a899f020502a47f3d9a99.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c2b1ac90ffb64eb15b6c",
+    "url": "/newclicky/static/js/runtime~main.c2b1ac90.js"
+  },
+  {
+    "revision": "2afedc08d7b5629bde1e",
+    "url": "/newclicky/static/js/main.2afedc08.chunk.js"
+  },
+  {
+    "revision": "ba2021e67c053dfd7e1d",
+    "url": "/newclicky/static/js/2.ba2021e6.chunk.js"
+  },
+  {
+    "revision": "2afedc08d7b5629bde1e",
+    "url": "/newclicky/static/css/main.4e5e0e05.chunk.css"
+  },
+  {
+    "revision": "3c0e6dd44959ca4a5bd5d2c24b951cf4",
+    "url": "/newclicky/index.html"
+  }
+];
